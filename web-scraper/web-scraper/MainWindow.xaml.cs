cdabb09c0fa95851f07ad585395a64c75215f478
RyanMcKelvey <ryanmckelvey98@gmail.com>
2021-04-06 13:50:32 +0100

    Adding web scraping capabilities to main window
    Also adding in asynchronous download of web pages

--- web-scraper/web-scraper/MainWindow.xaml.cs
+++ web-scraper/web-scraper/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+
+//Dependencies
+using HtmlAgilityPack;
 
 namespace web_scraper
 {
@@ -23,6 +27,43 @@ namespace web_scraper
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //Main scraping function here
+        public void BeginScrape(object sender, RoutedEventArgs e)
+        {
+            txt.Document.Blocks.Clear();
+            var html = @"http://html-agility-pack.net/";
+            DownloadWebPage(html);
+        }
+
+        public void DownloadWebPage(string html)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                Uri _uri = new Uri(html, UriKind.Absolute);
+                wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadedWebHandler);
+                wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressHandler);
+                wc.DownloadStringAsync(_uri);
+            }
+        }
+        public void DownloadProgressHandler(Object sender, DownloadProgressChangedEventArgs e)
+        {
+            prg.Value = e.ProgressPercentage;
+        }
+
+        public void DownloadedWebHandler(Object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (!e.Cancelled && e.Error == null)
+            {
+                string textString = (string)e.Result;
+                HtmlParse(textString);
+            }
+        }
+        public void HtmlParse(string t) {
+            var html = new HtmlDocument();
+            html.LoadHtml(t);
+            txt.AppendText(html.DocumentNode.OuterHtml);    
         }
     }
 }
