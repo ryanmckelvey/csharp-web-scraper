3cdf0f8716ff3270bfbec1dd1e910371bc5f8be1
RyanMcKelvey <ryanmckelvey98@gmail.com>
2021-04-11 16:02:13 +0100

    added background working and proper cancellation

--- web-scraper/web-scraper-v2/MainPage.xaml.cs
+++ web-scraper/web-scraper-v2/MainPage.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -14,8 +13,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Web.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.ComponentModel;
 
 using HtmlAgilityPack;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -26,34 +30,58 @@ namespace web_scraper_v2
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker();
         public MainPage()
         {
             this.InitializeComponent();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.DoWork += new DoWorkEventHandler(DownloadWebPage);
         }
         //Main scraping function here
         public void BeginScrape(object sender, RoutedEventArgs e)
         {
             txt.Text = "";
             prg.Value = 0;
-            var html = @"http://html-agility-pack.net/";
-            DownloadWebPage(html);
+            cancelBtn.IsEnabled = true;
+            backgroundWorker1.RunWorkerAsync();
         }
 
-        public void DownloadWebPage(string html)
+        public void DownloadWebPage(object sender, System.EventArgs e)
         {
+            var html = @"http://html-agility-pack.net/";
             Uri _uri = new Uri(html, UriKind.Absolute);
             using (WebClient wc = new WebClient())
             {
                 wc.Dispose();
                 wc.DownloadStringAsync(_uri);
-                wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressHandler);
-                wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadedWebHandler);
+                wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressHandlerAsync);
+                if (backgroundWorker1.CancellationPending != true)
+                {
+                    wc.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadedWebHandler);
+                }
             }
         }
 
-        public void DownloadProgressHandler(Object sender, DownloadProgressChangedEventArgs e)
+        public async void CancelScrape(object sender, RoutedEventArgs e)
         {
-            prg.Value = e.ProgressPercentage;
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            {
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    cancelBtn.IsEnabled = false;
+                });
+                backgroundWorker1.CancelAsync();
+            }
+        }
+
+        public async void DownloadProgressHandlerAsync(Object sender, DownloadProgressChangedEventArgs e)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+             {
+                 prg.Value = e.ProgressPercentage;
+             });
+
         }
 
         public void DownloadedWebHandler(Object sender, DownloadStringCompletedEventArgs e)
@@ -63,12 +91,20 @@ namespace web_scraper_v2
                 string textString = (string)e.Result;
                 HtmlParse(textString);
             }
+            else
+            {
+                HtmlParse("Error");
+            }
         }
-        public void HtmlParse(string t)
+        public async void HtmlParse(string t)
         {
             var html = new HtmlDocument();
             html.LoadHtml(t);
-            txt.Text = html.DocumentNode.OuterHtml;
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                txt.Text = html.DocumentNode.OuterHtml;
+            });
+            
         }
     }
 }
